f1b691f14fcf5bb49d935471bdbc689c93344006
Terence Martin <github@nurdz.com>
2021-01-26 22:55:34 -0800

    Enhance binding creation and cleanup
    This commit makes a few enhancements to how key bindings are created at
    project load time and also cleans up bindings when projects close as
    well, to try and keep things tidy.
    
    The salient points are:
    
      * Capture the package name the package is installed under instead of
        presuming a hard coded one.
    
      * When a project is opened, if it has no bindings in it the existing
        bindings for that project (if any) are removed; this makes sure that
        if a project is modified while Sublime is not running, that older
        bindings don't remain.
    
      * When a project is closed, remove the bindings for the current
        platform (if there are any); this keeps things tidy.

--- project_keys.py
+++ project_keys.py
@@ -11,6 +11,8 @@ _platform_name = {
     'linux': 'Linux',
 }[sublime.platform()]
 
+_pkg_name = __name__.split('.')[0]
+
 
 def plugin_loaded():
     """
@@ -20,11 +22,53 @@ def plugin_loaded():
     os.makedirs(keymap_dir(), exist_ok=True)
 
 
-def keymap_dir():
+def project_name(window):
     """
-    Obtain the folder that we want to generate our keymap files into.
+    Return the file name (without extension) for the project currently
+    available in the given window.
     """
-    return os.path.join(sublime.packages_path(), "ProjectSpecificKeys", "keymaps")
+    if window.project_file_name() is None:
+        return None
+
+    return os.path.split(window.project_file_name())[1]
+
+
+def keymap_dir(project_filename=None):
+    """
+    Obtain the folder that we want to generate our keymap files into for the
+    provided project.
+    """
+    if project_filename:
+        project_filename = os.path.splitext(project_filename)[0]
+
+    return os.path.join(sublime.packages_path(), _pkg_name, "keymaps", project_filename or "")
+
+
+def keymap_file_path(project_filename):
+    """
+    Get the fully qualified filename for a key binding file on the current
+    platform for the given project filename.
+    """
+    filename = "Default (%s).sublime-keymap" % _platform_name
+    return os.path.join(keymap_dir(project_filename), filename)
+
+
+def cleanup_keymap(filename=None, folder=None):
+    """
+    Delete the provided keymap file and, if provided, also remove the
+    given directory as well if provided.
+    """
+    if filename:
+        try:
+            os.unlink(filename)
+        except FileNotFoundError:
+            pass
+
+    if folder:
+        try:
+            os.rmdir(folder)
+        except (FileNotFoundError, OSError):
+            pass
 
 
 def add_context(keymap, project_name):
@@ -65,10 +109,8 @@ class ProjectSpecificEventListener(sublime_plugin.EventListener):
     def on_load_project(self, window):
         data = window.project_data() or {}
         keys = data.get("keys", [])
-        if not keys:
-            return
 
-        project = os.path.split(window.project_file_name())[1]
+        project = project_name(window)
 
         keymap = []
         for key in keys:
@@ -77,15 +119,24 @@ class ProjectSpecificEventListener(sublime_plugin.EventListener):
                 keymap.append(add_context(key, project))
 
         try:
-            folder = os.path.join(keymap_dir(), project)
-            if not os.path.isdir(folder):
+            folder = keymap_dir(project)
+            if keymap and not os.path.isdir(folder):
                 os.mkdir(folder)
 
-            filename = "Default (%s).sublime-keymap" % _platform_name
-            filename = os.path.join(folder, filename)
-            with open(filename, "w") as file:
-                file.write(json.dumps(keymap, indent=4))
+            filename = keymap_file_path(project)
+            if keymap:
+                with open(filename, "w") as file:
+                    file.write(json.dumps(keymap, indent=4))
+            else:
+                cleanup_keymap(filename, folder)
 
         except Exception as e:
             sublime.status_message("Error generating project specific bindings")
             raise e
+
+    def on_pre_close_project(self, window):
+        project = project_name(window)
+        folder = keymap_dir(project)
+        filename = keymap_file_path(project)
+
+        cleanup_keymap(filename, folder)
