b33d11846eb9faa4a3141694fa4a0abe4fb3277c
Terence Martin <github@nurdz.com>
2021-02-06 21:23:50 -0800

    Reapply or clean up key bindings on startup
    Since it is possible that while Sublime is not running a project could
    be modified to include key bindings, change bindings or remove bindings,
    it is important to ensure that the bindings are updated when Sublime is
    starting up.

--- project_keys.py
+++ project_keys.py
@@ -17,9 +17,13 @@ _pkg_name = __name__.split('.')[0]
 def plugin_loaded():
     """
     When the plugin loads,  make sure that there's a folder for storing the
-    keymap file that we're going to generate.
+    keymap file that we're going to generate and then check to see if any of
+    the current windows have projects in it and update their bindings as
+    appropriate.
     """
     os.makedirs(keymap_dir(), exist_ok=True)
+    for window in sublime.windows():
+        apply_keymaps(window)
 
 
 def project_name(window):
@@ -87,6 +91,45 @@ def add_context(keymap, project_name):
     return keymap
 
 
+def apply_keymaps(window):
+    """
+    Given a window, check it to see if it it contains a project, and if so
+    activate any key bindings present in it.
+
+    This will remove the key bindings for a project that used to have them
+    if it no longer does, which could happen if the project is changed by
+    some external factor, such as source control.
+    """
+    project = project_name(window)
+    if project is None:
+        return
+
+    data = window.project_data() or {}
+    keys = data.get("keys", [])
+
+    keymap = []
+    for key in keys:
+        platform = key.pop("platform", None)
+        if platform != '!' + _platform_name or platform == _platform_name:
+            keymap.append(add_context(key, project))
+
+    try:
+        folder = keymap_dir(project)
+        if keymap and not os.path.isdir(folder):
+            os.mkdir(folder)
+
+        filename = keymap_file_path(project)
+        if keymap:
+            with open(filename, "w") as file:
+                file.write(json.dumps(keymap, indent=4))
+        else:
+            cleanup_keymap(filename, folder)
+
+    except Exception as e:
+        sublime.status_message("Error generating project specific bindings")
+        raise e
+
+
 class ProjectSpecificEventListener(sublime_plugin.EventListener):
     def on_query_context(self, view, key, operator, operand, match_all):
         if key != "project":
@@ -107,32 +150,7 @@ class ProjectSpecificEventListener(sublime_plugin.EventListener):
         return None
 
     def on_load_project(self, window):
-        data = window.project_data() or {}
-        keys = data.get("keys", [])
-
-        project = project_name(window)
-
-        keymap = []
-        for key in keys:
-            platform = key.pop("platform", None)
-            if platform != '!' + _platform_name or platform == _platform_name:
-                keymap.append(add_context(key, project))
-
-        try:
-            folder = keymap_dir(project)
-            if keymap and not os.path.isdir(folder):
-                os.mkdir(folder)
-
-            filename = keymap_file_path(project)
-            if keymap:
-                with open(filename, "w") as file:
-                    file.write(json.dumps(keymap, indent=4))
-            else:
-                cleanup_keymap(filename, folder)
-
-        except Exception as e:
-            sublime.status_message("Error generating project specific bindings")
-            raise e
+        apply_keymaps(window)
 
     def on_pre_close_project(self, window):
         project = project_name(window)
